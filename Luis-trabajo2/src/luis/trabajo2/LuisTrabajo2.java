d010e78a16dea8983e1b78139907760ccca25c92
LuIsMoReNo7 <1483853@senati.pe>
2023-03-10 09:03:31 -0500

    Hello again world

---
+++ Luis-trabajo2/src/luis/trabajo2/LuisTrabajo2.java
@@ -0,0 +1,13 @@
+
+package luis.trabajo2;
+
+
+public class LuisTrabajo2 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello again world");
+    }
+    
+}
