2fb1d0701c1abc498406e9a9e1fe1e1637d27e0f
LuIsMoReNo7 <1483853@senati.pe>
2023-03-11 11:00:20 -0500

    prueba 2 de suma, me falto el resultado

---
+++ Sabado-Java-11-03-23/trabajo-suma.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class suma {
+    public static void main(String[] args) {
+        int a, b = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("introduce dos numeros: ");
+        a = s.nextInt();
+        b = s.nextInt();
+        System.out.println("la suma de a + b es: ");
+    }
+}
