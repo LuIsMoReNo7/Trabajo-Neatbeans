b4cc2f6876faffedf6adb84a41ab92339223f752
LuIsMoReNo7 <1483853@senati.pe>
2023-03-11 10:51:09 -0500

    prueba 1 con public class

---
+++ Sabado-Java-11-03-23/Prueba 1 suma.java
@@ -0,0 +1,14 @@
+import javax.naming.spi.DirStateFactory.Result;
+
+public class main {
+    public static void main(String[]args) {
+        int numero1 = 0;
+        int numero2 = 0;
+
+        System.out.println("")
+
+        Resultado = numero1+numero2;
+
+        System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+    }
+}
