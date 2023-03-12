55db2b60fddc713afcd4779af7f023135ae248ea
LuIsMoReNo7 <1483853@senati.pe>
2023-03-12 18:51:31 -0500

    Trabajo Suma de la clase del sabado

---
+++ Sabado-Java-11-03-23/suma_de_valores.java.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; // Es una clase que nos permite obtener la entrada de datos primitivos
+
+public class suma_de_valores {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); // Declaramos la clase "Scanner"
+        // Pedimos el primer numero
+        System.out.println("Ingrese el primer numero a sumar: ");
+        // "nextInt()" lee valores int introducidos por el usario
+        int numero1 = entrada.nextInt();
+        // pedimos el segundo numero
+        System.out.println("Ingrese el segundo numero a sumar: ");
+        // "nextInt()" lee valores int introducidos por el usario
+        int numero2 = entrada.nextInt();
+        // hacemos la suma
+        int resultado = numero1 + numero2;
+        // Imprimos el valor de numero1 , numero2 y por ultimo imprimos el resultado de
+        // los 2
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es igual a " + resultado);
+
+    }
+
+}
---
+++ Sabado-Java-11-03-23/trabajo_suma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class trabajo_suma {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); //Declaramos la palabra scanner
+        //ingresamos el primer valor
+        System.out.println("");
+        // "nextInt()" lee los valores ingresdos por el usuario
+        int numero1 = entrada.nextInt();
+        //Ingresamos el segundo valor
+        System.out.println("Ingrese el segundo valor");
+        // "nextInt()" lee los valores ingresdos por el usuario
+        int numero2 = entrada.nextInt();
+        //Realizamos la suma
+        int resultado = numero1 + numero2;
+        // Imprimos los valores la suma
+        System.out.println("La suma de" + numero1 + " y " + numero2 + "es igual  a" + resultado);
+      
+    }
+}
+    
+
+    
